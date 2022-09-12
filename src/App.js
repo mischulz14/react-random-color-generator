269150d5814a9aa64f0ea4728fa117aaa024c490
Michael Schulz <mi.schulz@hotmail.com>
2022-09-12 14:34:00 +0200

    Change styling

--- src/App.js
+++ src/App.js
@@ -4,61 +4,6 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const Heading = styled.h1`
-  margin-bottom: 3rem;
-`;
-
-const GlobalContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  text-align: center;
-  height: 100vh;
-  width: 100%;
-`;
-
-const Container = styled.div`
-  width: 25rem;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 4rem;
-`;
-
-const Input = styled.input`
-  border: 0;
-  outline: 0;
-  border-bottom: 2px solid black;
-  padding: 0.3rem 1rem;
-  margin-bottom: 2rem;
-`;
-
-const Button = styled.button`
-  padding: 1rem 1.5rem;
-  background-color: white;
-  border-radius: 10px;
-  font-size: 1.3rem;
-  margin-bottom: 2rem;
-  transition: all 0.2s;
-  box-shadow: 2px 2px 1px 1px black;
-  &:hover {
-    transform: translateY(-2px);
-  }
-
-  &:active {
-    transform: translate(2px);
-    box-shadow: 0px 0px 0px 0px black;
-  }
-`;
-
-const Generated = styled.div`
-  text-align: center;
-  padding: 4rem 8rem;
-  border-radius: 10px;
-  transition: background-color 0.6s ease-out;
-  border: 1px solid rgba(0, 0, 0, 0.136);
-`;
-
 function App() {
   const [color, setColor] = useState('none');
   const [hue, setHue] = useState('');
@@ -88,35 +33,35 @@ function App() {
   }
 
   return (
-    <GlobalContainer className="App">
-      <Container>
-        <Heading>Generate Random Colors</Heading>
-        <Input
+    <div className="global">
+      <div className="container">
+        <h1>Generate Random Colors</h1>
+        <input
           placeholder="set luminosity"
           id="lum"
           value={luminosity}
           onChange={(e) => handleHLuminosityChange(e)}
         />
-        <Input
+        <input
           placeholder="set hue"
           id="hue"
           value={hue}
           onChange={(e) => handleHueChange(e)}
         />
-        <Button onClick={handleGenerateColor} className="generate">
+        <button onClick={handleGenerateColor} className="generate">
           Generate
-        </Button>
-        <Generated
+        </button>
+        <div
+          className="generated"
           style={{
             backgroundColor: color,
             color: contrastColor({ bgColor: color }),
           }}
-          className="generated"
         >
           Generated Color: {color}
-        </Generated>
-      </Container>
-    </GlobalContainer>
+        </div>
+      </div>
+    </div>
   );
 }
 
